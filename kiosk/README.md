5134cd7801d8fb57591410a625273f69afcabbe0
Roberto Tyley <roberto.tyley@theguardian.com>
2019-05-08 16:15:14 +0100

    Removing these lines doesn't stop the Chromium from showing does it...

--- kiosk/README.md
+++ kiosk/README.md
@@ -2,3 +2,9 @@
 
 https://pimylifeup.com/raspberry-pi-kiosk/
 
+The version of Chromium in Raspbian currently has uBlock Origin enabled -
+this seems to block XHR requests like 
+https://dashboard.ophan.co.uk/graph/pageviews/data , which means the graph
+lines won't show.
+
+Disable uBlock Origin to get the graph working again.
--- kiosk/kiosk.sh
+++ kiosk/kiosk.sh
@@ -8,9 +8,4 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://pimylifeup.com https://www.adafruit.com &
-
-while true; do
-   xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
-   sleep 10
-done
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://www.theguardian.com/uk &
