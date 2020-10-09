acfb14ab8718751e0a19d19e8bb27aefaae6d322
Leigh-Anne Mathieson <3072877+lmath@users.noreply.github.com>
2020-10-09 12:17:09 +0100

    Stop Chromium asking to be the default browser
    When we ask Chromium to open a page in kiosk mode from this script, we
    disable dialogs that will clutter the display of the page, and this
    change ensures that we also disable Chromium asking to be the default
    browser, because this is also clutter.

--- kiosk/kiosk.sh
+++ kiosk/kiosk.sh
@@ -8,7 +8,7 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://www.theguardian.com/uk &
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --no-default-browser-check --kiosk https://www.theguardian.com/uk &
 
 while true; do # as soon as this script exits, the browser window will be killed...
    sleep 10
